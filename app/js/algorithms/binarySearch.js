d0c327e9a9f04431e63844ba753d7bb225c8147c
Andrew <betelgeuse.as@gmail.com>
2019-02-24 16:27:35 +0200

    Computational complexity

--- app/js/algorithms/binarySearch.js
+++ app/js/algorithms/binarySearch.js
@@ -3,17 +3,17 @@ const binarySearch = (value, list) => {
   let high = list.length - 1; // конечный индекс
   let position = -1;
 
-  while (low <= high) {
-    let middle = Math.floor((low + high) / 2);
+  while (low <= high) { //пока эта часть не сократится до одного элемента...
+    let middle = Math.floor((low + high) / 2); //... проверяем средний элемент
     if (list[middle] === value) {
       return middle;
-    } else if (list[middle] > value){ // значение в нижней части списка
+    } else if (list[middle] > value){ //значение в нижней части массива
       high = middle - 1;
-    } else { // значение в верхней части списка
+    } else { //значение в верхней части массива
       low = middle + 1;
     }
   }
-  return position;
+  return position; //значение не существует
 };
 
 //console.log(binarySearch(3, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
